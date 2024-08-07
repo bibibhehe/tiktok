8b3c34564484b6fdd1eab32e01f741b6df6edf22
anhbd <buiducanh931@gmail.com>
2024-08-07 09:33:03 +0700

    2

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ function App() {
   const items = ["Item 1", "Item 2", "Item 3"];
   const [mounted, setMounted] = useState(false);
   return (
+    <>
+    <Content />
     <ul>
       {items.map((item, index) => (
         <li key={index}>{item}</li>
       ))}
     </ul>
+    </>
+    
   );
 }
 
